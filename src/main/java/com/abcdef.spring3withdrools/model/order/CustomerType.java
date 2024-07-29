edcd0e3c7a4b4bc1b429471c853d57be955c56b8
yangli136 <53447457+yangli136@users.noreply.github.com>
2024-07-28 23:51:07 -0700

    Delete src/main/java/com/abcdef.spring3withdrools directory

--- src/main/java/com/abcdef.spring3withdrools/model/order/CustomerType.java
+++
@@ -1,9 +0,0 @@
-package com.abcdef.springboot3withdrools.model.order;
-
-public enum CustomerType {
-    LOYAL, NEW, DISSATISFIED;
-
-    public String getValue() {
-        return this.toString();
-    }
-}
--- src/main/java/com/abcdef.spring3withdrools/service/greeting/GreetingStringValidationService.java
+++
@@ -1,8 +0,0 @@
-package com.abcdef.springboot3withdrools.service.greeting;
-
-import java.util.List;
-
-public interface GreetingStringValidationService {
-
-    List<String> validGreetingString(String greetingString, Integer length);
-}
