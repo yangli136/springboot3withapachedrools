64f8bb4bad3ef63859034c47f1ed6cc1f78561cf
yangli136 <53447457+yangli136@users.noreply.github.com>
2024-07-28 22:49:52 -0700

    Add files via upload

---
+++ src/main/java/com/abcdef.spring3withdrools/rule/HelloWorldUnit.java
@@ -0,0 +1,62 @@
+package com.abcdef.springboot3withdrools.rule;
+
+import static org.drools.model.Index.ConstraintType.EQUAL;
+import static org.drools.model.Index.ConstraintType.GREATER_THAN;
+import static org.drools.model.Index.ConstraintType.LESS_THAN;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.ruleunits.api.DataSource;
+import org.drools.ruleunits.api.DataStore;
+import org.drools.ruleunits.dsl.RuleUnitDefinition;
+import org.drools.ruleunits.dsl.RulesFactory;
+import org.springframework.stereotype.Service;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Service
+@Getter
+@ToString
+public class HelloWorldUnit implements RuleUnitDefinition {
+
+    private final DataStore<String> strings; // DataStore where you add String fact
+    private final DataStore<Integer> ints; // DataStore where you add Integer fact
+    private final List<String> results = new ArrayList<>(); // Store results. In traditional DRL, it is called `global`
+
+    public HelloWorldUnit() {
+        this(DataSource.createStore(), DataSource.createStore());
+    }
+
+    public HelloWorldUnit(DataStore<String> strings, DataStore<Integer> ints) {
+        this.strings = strings;
+        this.ints = ints;
+    }
+
+    @Override
+    public void defineRules(RulesFactory rulesFactory) {
+        // /strings[ this == "Hello World" ]
+        rulesFactory.rule().on(strings).filter(EQUAL, "Hello World") // when no extractor is provided "this" is implicit
+                .execute(results, r -> r.add("greeting matches [Hello World]")); // the consequence can ignore the matched facts
+
+        // /strings[ length > 5 ]
+        rulesFactory.rule().on(strings) // since the datasource has been already initialized its class can be inferred without the need of explicitly passing it
+                .filter(s -> s.length(), GREATER_THAN, 5) // when no property name is provided it's impossible to generate indexes and property reactivity
+                .execute(results, (r, s) -> r.add("greeting length is greater than 5:" + s.toUpperCase())); // this consequence also uses the matched fact
+
+        // /strings[ length < 5 ]
+        rulesFactory.rule("MyRule") // it is possible to optionally set a name for the rule
+                .on(strings).filter("length", s -> s.length(), LESS_THAN, 5) // providing the name of the property used in the constraint allows index and property reactivity generation
+                .execute(results, r -> r.add("greeting length is less than 5."));
+
+        // $s: /strings[ length > 5 ]
+        // /ints[ this > 5, this == $s.length ]
+        rulesFactory.rule().on(strings).filter("length", s -> s.length() > 5) // it is also possible to use a plain lambda predicate, but in this case no index can be generated
+                .join(rule -> rule.on(ints) // creates a new pattern ...
+                        .filter(GREATER_THAN, 5) // ... add an alpha filter to it
+                ) // ... and join it with the former one
+                .filter(EQUAL, String::length) // this filter is applied to the result of the join, so it is a beta constraint
+                .execute(results, (r, s, i) -> r.add("String '" + s + "' is " + i + " characters long.")); // the consequence captures all the joined variables positionally
+    }
+}
